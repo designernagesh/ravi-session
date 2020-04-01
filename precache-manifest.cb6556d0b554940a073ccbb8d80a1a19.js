ca8e705fa136ac5a337c1eee4b481c1e53546809
Nagesh <nagesh.k@tecnolynx.com>
2020-04-01 22:07:08 +0530

    Updates

---
+++ precache-manifest.cb6556d0b554940a073ccbb8d80a1a19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4df897e5bea703940a0447f3dcf61d",
+    "url": "/ravi-session/index.html"
+  },
+  {
+    "revision": "230a95b6a32d929dca48",
+    "url": "/ravi-session/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bd7517d061c845327bcf",
+    "url": "/ravi-session/static/css/main.fd8f9aee.chunk.css"
+  },
+  {
+    "revision": "230a95b6a32d929dca48",
+    "url": "/ravi-session/static/js/2.9bf13279.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravi-session/static/js/2.9bf13279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7517d061c845327bcf",
+    "url": "/ravi-session/static/js/main.eb7bd5a1.chunk.js"
+  },
+  {
+    "revision": "90517f31c80ce4fcc2b8",
+    "url": "/ravi-session/static/js/runtime-main.3390d33b.js"
+  },
+  {
+    "revision": "63de6e456bf02ef2df5e82d8e159d302",
+    "url": "/ravi-session/static/media/img1.63de6e45.jpg"
+  }
+]);
